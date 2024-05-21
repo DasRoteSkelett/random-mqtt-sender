3e2e3fd98935f6253b54848895df84aa44594e56
Matthias Schoepfer <matthias.schoepfer@googlemail.com>
2024-05-21 13:51:36 +0200

    [cleanup] removed unused stuff, removed wait()
    Removed some unused code from the example, removed the wait() to
    not wait on successful delivery.
    
    Signed-off-by: Matthias Schoepfer <matthias.schoepfer@googlemail.com>

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include <mqtt/async_client.h>
 
 #include <iostream>
-#include <atomic>
 #include <thread>
 
 constexpr const char* SERVER_ADDRESS{ "tcp://192.168.7.1:1883" };
@@ -28,50 +27,6 @@ public:
 	}
 };
 
-/////////////////////////////////////////////////////////////////////////////
-
-/**
- * A base action listener.
- */
-class action_listener : public virtual mqtt::iaction_listener
-{
-protected:
-	void on_failure(const mqtt::token& tok) override {
-		std::cout << "\tListener failure for token: "
-			<< tok.get_message_id() << std::endl;
-	}
-
-	void on_success(const mqtt::token& tok) override {
-		std::cout << "\tListener success for token: "
-			<< tok.get_message_id() << std::endl;
-	}
-};
-
-/////////////////////////////////////////////////////////////////////////////
-
-/**
- * A derived action listener for publish events.
- */
-class delivery_action_listener : public action_listener
-{
-	std::atomic<bool> done_;
-
-	void on_failure(const mqtt::token& tok) override {
-		action_listener::on_failure(tok);
-		done_ = true;
-	}
-
-	void on_success(const mqtt::token& tok) override {
-		action_listener::on_success(tok);
-		done_ = true;
-	}
-
-public:
-	delivery_action_listener() : done_(false) {}
-	[[nodiscard]] bool is_done() const { return done_; }
-};
-
-
 int main() {
 
     const int QOS = 2;
@@ -90,20 +45,14 @@ int main() {
 		conntok->wait();
 		std::cout << "  ...OK" << std::endl;
 
-		// First use a message pointer.
+		// Sending lots of messages, without waiting for delivery
 		for (int i=0;i < 100000;++i) {
 			std::cout << "Publishing message no " << i << std::endl;
-			std::string msg = "Sending message " + std::to_string(i);
+			const std::string msg = "Sending message " + std::to_string(i);
 			auto pubmsg = mqtt::make_message(TOPIC,msg.c_str(),msg.length(),QOS,true);
-			client.publish(pubmsg)->wait();
-			std::cout << " ....OK" <<std::endl;
+			client.publish(pubmsg);
 			std::this_thread::sleep_for(std::chrono::milliseconds(100));
-
 		}
-
-
-		// Double check that there are no pending tokens
-
 		// Disconnect
 		std::cout << "\nDisconnecting..." << std::endl;
 		client.disconnect()->wait();
